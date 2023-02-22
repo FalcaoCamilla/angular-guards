96b5a3946471d26bcc831e9e128ad5d55a2ce21d
FalcaoCamilla <camilla@beltsistemas.com.br>
2023-02-22 19:37:40 -0300

    canDeactivateGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AccountComponent } from './shared/account/account.component';
+import { CanActiveGuard } from './shared/guards/can-active.guard';
+import { CanDeactivateGuard } from './shared/guards/can-deactivate.guard';
+import { HomeComponent } from './shared/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'account', component: AccountComponent,
+  canActivate: [CanActiveGuard], canDeactivate: [CanDeactivateGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/account/account.component.ts
+++ src/app/shared/account/account.component.ts
@@ -12,4 +12,11 @@ export class AccountComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public exit() {
+    if(confirm("Você quer sair?")){
+      return true;
+    }
+    return false;
+  }
+
 }
